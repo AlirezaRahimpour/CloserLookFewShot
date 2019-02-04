787570bc48cd694d626cb19b129638ed1f7be11b
Flood Sung <floodsung@Floods-MacBook-Pro.local>
2019-02-04 15:42:47 +0800

    senet

--- filelists/miniImagenet/write_miniImagenet_filelist.py
+++ filelists/miniImagenet/write_miniImagenet_filelist.py
@@ -16,7 +16,7 @@ dataset_list = ['base', 'val', 'novel']
 cl = -1
 folderlist = []
 
-datasetmap = {'base':'train_val','val':'val','novel':'test'};
+datasetmap = {'base':'train','val':'val','novel':'test'};
 filelists = {'base':{},'val':{},'novel':{} }
 filelists_flat = {'base':[],'val':[],'novel':[] }
 labellists_flat = {'base':[],'val':[],'novel':[] }
